b86eb56cfe6eaee01d610021ef3122f6f69007fe
Jürgen <lutjuc11@htlkaindorf.at>
2015-04-29 09:26:06 +0200

    hopefully javafx works

---
+++ src/client/StartApplication.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Juergen
+ */
+public class StartApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        Scene scene = new Scene(root);
+        
+        scene.getStylesheets().add("css/format.css");
+        
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/client/StartScreenController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Juergen
+ */
+public class StartScreenController implements Initializable {
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private Button btnStart;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    public void onStart(ActionEvent evt) throws IOException, InterruptedException {
+        Stage stage = (Stage) btnStart.getScene().getWindow();
+        stage.close();
+        Parent root = FXMLLoader.load(getClass().getResource("LoadingScreen.fxml"));
+        Scene scene = new Scene(root);
+        
+        scene.getStylesheets().add("css/format.css");
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void onOptions(ActionEvent evt) {
+        JOptionPane.showMessageDialog(null, "Options");
+    }
+
+    @FXML
+    public void onQuit(ActionEvent evt) {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+    }
+    
+}
